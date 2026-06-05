13a997aba86cf8e4cb1467ab23e2db76eb67f2d6
chknd1nner <martin.kuek@gmail.com>
2026-06-05 15:01:38 +1000

    Removed FamilyOS from this repo in favour of a dedicated repo. FamilyOS now resides in https://github.com/chknd1nner/FamilyOS

--- services/familyos/src/pi/session-paths.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-
-export function encodeSessionCwd(cwd: string) {
-  return `--${cwd.replace(/^[/\\]/, "").replace(/[/\\:]/g, "-")}--`;
-}
-
-export function getSharedSessionDir(cwd: string, agentDir: string) {
-  return path.join(agentDir, "sessions", encodeSessionCwd(cwd));
-}
